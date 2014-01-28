34cc05738e5a711e05c36375d0f547c8e1648f73
Jan Alexander Stormark Bremnes <janbremnes@gmail.com>
2014-01-28 16:59:46 +0100

    added pycsp

--- project/build/include/csp/csp_autoconfig.h
+++ project/build/include/csp/csp_autoconfig.h
@@ -3,7 +3,7 @@
 #ifndef _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
 #define _INCLUDE_CSP_CSP_AUTOCONFIG_H_WAF
 
-#define GIT_REV "143309c"
+#define GIT_REV "390d4eb"
 /* #undef CSP_FREERTOS */
 #define CSP_POSIX 1
 /* #undef CSP_WINDOWS */
